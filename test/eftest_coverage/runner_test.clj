4029816c1b5843a836cf2c41e00c23f96f156823
Andrey Bogoyavlensky <abogoyavlensky@gmail.com>
2020-10-16 13:53:48 +0300

    Fix testing namespace

--- test/eftest_coverage/runner_test.clj
+++ test/eftest_coverage/runner_test.clj
@@ -1,6 +1,5 @@
 (ns eftest-coverage.runner-test
-  (:require [clojure.test :refer :all]
-            [eftest-coverage.runner :refer :all]))
+  (:require [clojure.test :refer :all]))
 
 (deftest a-test
   (testing "FIXME, I'm successful."
